14b44d6c8e4930c118acf3d743d4ee29b0caf31a
Kevin Cullen <kevin@kevincullen.me>
2015-07-21 19:14:28 -0500

    media query 1

---
+++ js/main (KevinCullen's conflicted copy 2015-07-21).js
@@ -0,0 +1,36 @@
+/*$(document).ready(function () {
+    
+    var hideNav = function () {
+        $.find(".mainNav").animate({left: "15px", opacity: "1"}, "fast"); };
+    var showNav = function () {
+        $.find(".mainNav").animate({right: "15px", opacity: "0"}, "fast");        
+    };
+   // jQuery methods go here...
+    $(".contactForm").on("focusin", "input", showError);
+    $(".contactForm").on("focusout", "input", hideError);    
+    $(".contactForm").on("focusin", "textarea", showError);
+    $(".contactForm").on("focusout", "textarea", hideError
+  
+});
+  */
+  
+
+
+  
+
+
+$(document).ready(function () {    
+    var iScrollPos = 0;
+
+  $(window).scroll(function () {
+    var iCurScrollPos = $(this).scrollTop();
+    if (iCurScrollPos > iScrollPos) {
+      //Scrolling Down
+      $(".mainNav").slideUp("slow"); 
+    } else {
+      //Scrolling Up
+        $(".mainNav").slideDown("slow");
+        }
+    iScrollPos = iCurScrollPos;
+});  
+});
